ee6e04ef7833d868a8e769117b3f57259f54e780
Kia <kiaplaysgamer@gmail.com>
2024-06-21 12:51:30 +0330

    dateTaskList added

--- app/src/main/java/com/example/testproject/date/Date.kt
+++ app/src/main/java/com/example/testproject/date/Date.kt
@@ -3,10 +3,13 @@ package com.example.testproject.date
 import android.os.Parcel
 import android.os.Parcelable
 import com.example.testproject.task.Task
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Date(
     var day: String,
     var date: Int,
     var isSelected : Boolean = false,
     var dateTasksList : ArrayList<Task> = arrayListOf()
     //todo kianoosh add list of tasks
-)
+):Parcelable
